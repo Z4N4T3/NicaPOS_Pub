7ad9ea3cc9de423f620db8b1f736a45edc070df7
Jurgen <jurgenq75@gmail.com>
2024-09-12 23:10:55 -0600

    toggle

--- js/inventario.js
+++ js/inventario.js
@@ -1,1 +1,22 @@
-import {}
+const rightBtn = document.querySelector('#add-item')
+const lightbox= document.querySelector('.lightbox')
+const lbContainer =document.querySelector('.lightbox-container')
+const icon = document.querySelector('#icon')
+
+rightBtn.addEventListener('click',exitLightbox)
+
+// lightbox.addEventListener('click',exitLightbox)
+
+
+function exitLightbox(){
+    let posClick = event.target;
+    if (posClick.classList.contains('bi-x-square-fill')){
+        lightbox.style.display='none'
+        icon.classList.remove('bi-x-square-fill')
+        icon.classList.add('bi-plus-square-fill')
+    }else if(posClick.classList.contains('bi-plus-square-fill')){
+        lightbox.style.display='flex'
+        icon.classList.remove('bi-plus-square-fill')
+        icon.classList.add('bi-x-square-fill')
+    }
+}
